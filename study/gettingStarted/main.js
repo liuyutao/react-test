ffe2f2692110f36ef80cd12b76c1bd97d685642b
liuyutao <361292910@qq.com>
2015-11-30 13:37:30 +0800

    Signed-off-by: liuyutao <361292910@qq.com>

---
+++ study/gettingStarted/main.js
@@ -0,0 +1,18 @@
+/**
+ * 功能描述：
+ * 使用方法:
+ * 注意事件：
+ * 引入来源：  作用：
+ *
+ * Created by LiuYuTao on 2015/11/30.
+ */
+
+
+var react = require('react');
+
+var ReactDOM = require('react-dom');
+
+React.render(
+  <h1>Hello,world!</h1>,
+  document.getElementById('example')
+)
